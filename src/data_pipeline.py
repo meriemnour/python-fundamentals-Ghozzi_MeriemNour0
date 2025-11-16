bc4a6d0811df28737ff6c9d754134ae16651905f
Mimou <ghozzimeriemnour.com>
2025-11-16 18:03:09 +0100

    scientific article models

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -2,8 +2,12 @@
 
 # (personal data) get users from file -> fill the defaults -> put into database ->
 # load from database -> add a comment -> load to mongodb
+
 # books -> embed text content (for search) -> put metadata to relational database,
 # put text into (other database) -> load to mongo -> find concepts
-# scientific articles
+
+# scientific articles -> load scientific articles from csv(files): metadata and
+# filename -> put into relational database -> load files from disc -> put into mongo
+
 # images
 # (numerical data) stocks market
